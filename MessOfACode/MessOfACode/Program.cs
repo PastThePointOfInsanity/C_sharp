02d57f6690976bf8721d991a40f1853a3b5b8c20
PastThePointOfInsanity <levermakov666@gmail.com>
2022-10-08 13:06:09 +0300

    aaaa

---
+++ MessOfACode/MessOfACode/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessOfACode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите два числа через запятую");
+            var input = Console.ReadLine();
+            var numbers = input.Split(',');
+
+            foreach (var s in numbers)
+            {
+                int result = 0;
+                if (!int.TryParse(s, out result))
+                {
+                    Console.WriteLine("Ошибка, введите число");
+                }
+
+                Console.WriteLine(result);
+            }
+            Console.ReadLine();
+        }
+    }
+}
